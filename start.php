c1193933b5c9f3ff0328972b6b118f49708ab0cb
Matt Beckett <beck24@gmail.com>
2015-10-13 00:18:53 -0700

    settings for all languages

--- start.php
+++ start.php
@@ -1,158 +1,143 @@
 <?php
+
+namespace Beck24\FlexibleFriendlyTime;
+
+const PLUGIN_ID = 'flexible_friendlytime';
+
+elgg_register_event_handler('init', 'system', __NAMESPACE__ . '\\init');
+
 /**
- * Author: Matt Beckett
- * Website: http://clever-name.com
- * 
- * License: GPL2
+ *  Init
  */
+function init() {
+	elgg_register_plugin_hook_handler('format', 'friendly:time', __NAMESPACE__ . '\\format_friendlytime');
+}
 
+/**
+ * Format the friendly time
+ * 
+ * @param type $hook
+ * @param type $type
+ * @param type $return
+ * @param type $params
+ * @return type
+ */
+function format_friendlytime($hook, $type, $return, $params) {
 
-function flexible_friendlytime_init() {
-  elgg_register_plugin_hook_handler('format', 'friendly:time', 'flexible_friendlytime_hook_handler');
+	$language = get_current_language();
+
+	// get our language dependent settings
+	$format1 = elgg_get_plugin_setting('format1' . $language, PLUGIN_ID);
+	$format1override = elgg_get_plugin_setting('format1override' . $language, PLUGIN_ID);
+	$format2 = elgg_get_plugin_setting('format2' . $language, PLUGIN_ID);
+	$format2override = elgg_get_plugin_setting('format2override' . $language, PLUGIN_ID);
+	$breaktime = (int) elgg_get_plugin_setting('breaktime' . $language, PLUGIN_ID);
+	$offset = (int) elgg_get_plugin_setting('offset' . $language, PLUGIN_ID);
+
+	// use regular time for elgg default, modtime otherwise
+	$time = $params['time'];
+	$modtime = $time + (60 * 60 * $offset);
+
+	$format = 1;
+	if (!empty($breaktime) && ($params['time'] < (time() - 60 * 60 * $breaktime))) {
+		$format = 2;
+	}
+
+	if ($format == 1) {
+		if (empty($format1override)) {
+			$check = $format1;
+		} else {
+			return translate_friendlytime($format1override, $modtime);
+		}
+	}
+
+	if ($format == 2) {
+		if (empty($format2override)) {
+			$check = $format2;
+		} else {
+			return translate_friendlytime($format2override, $modtime);
+		}
+	}
+
+	// nothing set, keep as elgg default
+	if (empty($check)) {
+		$check = 1;
+	}
+
+	switch ($check) {
+		case 1:
+			// default, leave $return alone
+			break;
+		case 2:
+			$return = elgg_echo('flexible_friendlytime:format:1', array(translate_friendlytime('M j, Y', $modtime), translate_friendlytime('g:ia', $modtime)));
+			break;
+		default:
+			$return = translate_friendlytime($check, $modtime);
+			break;
+	}
+
+	return $return;
 }
 
 
+/**
+ * makes dates translatable to current language
+ * 
+ * @param type $dateformat
+ * @param type $time
+ * @return type
+ */
+function translate_friendlytime($dateformat, $time = NULL) {
+	if (empty($time) && $time !== 0) {
+		$time = time();
+	}
 
-function flexible_friendlytime_hook_handler($hook, $type, $return, $params) {
-  
-  // make sure we have a language file for the selected language
-  // otherwise default to english
-  $languagepaths = elgg_get_file_list(elgg_get_plugins_path() . "flexible_friendlytime/languages/", array(), array(), array('.php'));
-  $languages = array();
-  foreach($languagepaths as $langpath){
-    $languages[] = basename($langpath, '.php');
-  }
-  
-  if(elgg_is_logged_in()){
-    // logged in, use user language
-    if(!in_array(elgg_get_logged_in_user_entity()->language, $languages)){
-      $language = 'en';
-    }
-    else{
-      $language = elgg_get_logged_in_user_entity()->language;
-    }
-  }
-  else{
-    // not logged in, use site language
-    if(!in_array(elgg_get_config('language'), $languages)){
-      $language = 'en';
-    }
-    else{
-      $language = elgg_get_config('language');
-    } 
-  }
-  
-  // get our language dependent settings
-  $format1 = elgg_get_plugin_setting('format1'.$language, 'flexible_friendlytime');
-  $format1override = elgg_get_plugin_setting('format1override'.$language, 'flexible_friendlytime');
-  $format2 = elgg_get_plugin_setting('format2'.$language, 'flexible_friendlytime');
-  $format2override = elgg_get_plugin_setting('format2override'.$language, 'flexible_friendlytime');
-  $breaktime = elgg_get_plugin_setting('breaktime'.$language, 'flexible_friendlytime');
-  $offset = elgg_get_plugin_setting('offset'.$language, 'flexible_friendlytime');
-  
-  // use regular time for elgg default, modtime otherwise
-  $time = $params['time'];
-  $modtime = $time + (60*60*$offset);
-  
-  $format = 1;
-  if(!empty($breaktime) && ($params['time'] < (time() - 60*60*$breaktime))){
-    $format = 2;
-  }
+	$output = "";
+	for ($i = 0; $i < strlen($dateformat); $i++) {
+		$j = $i - 1;
 
-  if($format == 1){
-    if(empty($format1override)){
-      $check = $format1;
-    }
-    else{
-      return flexible_friendlytime_translate($format1override, $modtime);
-    }
-  }
-  
-  if($format == 2){
-    if(empty($format2override)){
-      $check = $format2;
-    }
-    else{
-      return flexible_friendlytime_translate($format2override, $modtime);
-    }
-  }
-  
-  // nothing set, keep as elgg default
-  if(empty($check)){
-    $check = 1;
-  }
-  
-  switch ($check) {
-    case 1:
-      // default, leave $return alone
-      break;
-    case 2:
-        $return = elgg_echo('flexible_friendlytime:format:1', array(flexible_friendlytime_translate('M j, Y', $modtime), flexible_friendlytime_translate('g:ia', $modtime)));
-      break;
-    default:
-        $return = flexible_friendlytime_translate($check, $modtime);
-      break;
-  }
-  
-  return $return;
+		if ($dateformat[$j] == "\\") {
+			$output .= $dateformat[$i];
+			continue;
+		}
+
+		if ($dateformat[$i] == "\\" && $dateformat[$j] != "\\") {
+			$output .= "";
+			continue;
+		}
+
+		if ($dateformat[$i] == " ") {
+			$output .= " ";
+			continue;
+		}
+
+		if ($dateformat[$i] == "D" && $dateformat[$j] != "\\") {
+			$output .= elgg_echo('flexible_friendlytime:shortday:' . date("D", $time));
+			continue;
+		}
+
+		if ($dateformat[$i] == "l" && $dateformat[$j] != "\\") {
+			$output .= elgg_echo('flexible_friendlytime:longday:' . date("l", $time));
+			continue;
+		}
+
+		if ($dateformat[$i] == "F" && $dateformat[$j] != "\\") {
+			$output .= elgg_echo('flexible_friendlytime:longmonth:' . date("F", $time));
+			continue;
+		}
+
+		if ($dateformat[$i] == "M" && $dateformat[$j] != "\\") {
+			$output .= elgg_echo('flexible_friendlytime:shortmonth:' . date("M", $time));
+			continue;
+		}
+
+		if (strtolower($dateformat[$i]) == "a" && $dateformat[$j] != "\\") {
+			$output .= elgg_echo('flexible_friendlytime:' . date($dateformat[$i], $time));
+			continue;
+		}
+
+		$output .= date($dateformat[$i], $time);
+	}
+
+	return $output;
 }
-
-//
-// This function makes dates translatable to current language
-function flexible_friendlytime_translate($dateformat, $time = NULL){
-  if(empty($time) && $time !== 0){
-    $time = time();
-  }
-  
-  $output = "";
-  for($i=0; $i<strlen($dateformat); $i++){
-    $j = $i - 1;
-    
-    if($dateformat[$j] == "\\"){
-      $output .= $dateformat[$i];
-      continue;
-    }
-    
-    if($dateformat[$i] == "\\" && $dateformat[$j] != "\\"){
-      $output .= "";
-      continue;
-    }
-    
-    if($dateformat[$i] == " "){
-      $output .= " ";
-      continue;
-    }
-    
-    if($dateformat[$i] == "D" && $dateformat[$j] != "\\"){
-      $output .= elgg_echo('flexible_friendlytime:shortday:' . date("D", $time));
-      continue;
-    }
-    
-    if($dateformat[$i] == "l" && $dateformat[$j] != "\\"){
-      $output .= elgg_echo('flexible_friendlytime:longday:' . date("l", $time));
-      continue;
-    }
-    
-    if($dateformat[$i] == "F" && $dateformat[$j] != "\\"){
-      $output .= elgg_echo('flexible_friendlytime:longmonth:' . date("F", $time));
-      continue;
-    }
-    
-    if($dateformat[$i] == "M" && $dateformat[$j] != "\\"){
-      $output .= elgg_echo('flexible_friendlytime:shortmonth:' . date("M", $time));
-      continue;
-    }
-    
-    if(strtolower($dateformat[$i]) == "a" && $dateformat[$j] != "\\"){
-      $output .= elgg_echo('flexible_friendlytime:' . date($dateformat[$i], $time));
-      continue;
-    }
-    
-    $output .= date($dateformat[$i], $time);
-  }
-  
-  return $output;
-}
-
-
-elgg_register_event_handler('init', 'system', 'flexible_friendlytime_init');
--- views/default/plugins/flexible_friendlytime/settings.php
+++ views/default/plugins/flexible_friendlytime/settings.php
@@ -1,57 +1,53 @@
 <?php
-/**
- * Author: Matt Beckett
- * Website: http://clever-name.com
- * 
- * License: GPL2
- */
-$languages = elgg_get_file_list(elgg_get_plugins_path() . "flexible_friendlytime/languages/", array(), array(), array('.php'));
 
-foreach($languages as $langpath){
-  $language = basename($langpath, '.php');
+namespace Beck24\FlexibleFriendlyTime;
+
+$languages = get_installed_translations();
+
+foreach($languages as $language => $label){
 ?>
 	<div style="padding: 10px; margin: 15px; border: 2px solid #343434">
-	<h1><?php echo elgg_echo('flexible_friendlytime:language:settings', array($language)); ?></h1>
+	<h1><?php echo elgg_echo('flexible_friendlytime:language:settings', array($label)); ?></h1>
 	<br><br>
   <?php
   $options_values = array(
     1 => elgg_echo('flexible_friendlytime:elgg:default'),
-    2 => elgg_echo('flexible_friendlytime:format:1', array(flexible_friendlytime_translate('M j, Y'), flexible_friendlytime_translate('g:ia'))),
-    'Y-m-d H:i' => flexible_friendlytime_translate('Y-m-d H:i'),
-    'm/d/Y - H:i' => flexible_friendlytime_translate('m/d/Y - H:i'),
-    'd/m/Y - H:i' => flexible_friendlytime_translate('d/m/Y - H:i'),
-    'Y/m/d - H:i' => flexible_friendlytime_translate('Y/m/d - H:i'),
-    'd.m.Y - H:i' => flexible_friendlytime_translate('d.m.Y - H:i'),
-    'm/d/Y - g:ia' => flexible_friendlytime_translate('m/d/Y - g:ia'),
-    'd/m/Y - g:ia' => flexible_friendlytime_translate('d/m/Y - g:ia'),
-    'Y/m/d - g:ia' => flexible_friendlytime_translate('Y/m/d - g:ia'),
-    'M j Y - H:i' => flexible_friendlytime_translate('M j Y - H:i'),
-    'j M Y - H:i' => flexible_friendlytime_translate('j M Y - H:i'),
-    'Y M j - H:i' => flexible_friendlytime_translate('Y M j - H:i'),
-    'M j Y - g:ia' => flexible_friendlytime_translate('M j Y - g:ia'),
-    'j M Y - g:ia' => flexible_friendlytime_translate('j M Y - g:ia'),
-    'Y M j - g:ia' => flexible_friendlytime_translate('Y M j - g:ia'),
-    'D, Y-m-d H:i' => flexible_friendlytime_translate('D, Y-m-d H:i'),
-    'D, m/d/Y - H:i' => flexible_friendlytime_translate('D, m/d/Y - H:i'),
-    'D, d/m/Y - H:i' => flexible_friendlytime_translate('D, d/m/Y - H:i'),
-    'D, Y/m/d - H:i' => flexible_friendlytime_translate('D, Y/m/d - H:i'),
-    'F j, Y - H:i' => flexible_friendlytime_translate('F j, Y - H:i'),
-    'j F, Y - H:i' => flexible_friendlytime_translate('j F, Y - H:i'),
-    'Y, F j - H:i' => flexible_friendlytime_translate('Y, F j - H:i'),
-    'D, m/d/Y - g:ia' => flexible_friendlytime_translate('D, m/d/Y - g:ia'),
-    'D, d/m/Y - g:ia' => flexible_friendlytime_translate('D, d/m/Y - g:ia'),
-    'D, Y/m/d - g:ia' => flexible_friendlytime_translate('D, Y/m/d - g:ia'),
-    'F j, Y - g:ia' => flexible_friendlytime_translate('F j, Y - g:ia'),
-    'j F Y - g:ia' => flexible_friendlytime_translate('j F Y - g:ia'),
-    'Y, F j - g:ia' => flexible_friendlytime_translate('Y, F j - g:ia'),
-    'j. F Y - G:i' => flexible_friendlytime_translate('j. F Y - G:i'),
-    'l, F j, Y - H:i' => flexible_friendlytime_translate('l, F j, Y - H:i'),
-    'l, j F, Y - H:i' => flexible_friendlytime_translate('l, j F, Y - H:i'),
-    'l, Y,  F j - H:i' => flexible_friendlytime_translate('l, Y,  F j - H:i'),
-    'l, F j, Y - g:ia' => flexible_friendlytime_translate('l, F j, Y - g:ia'),
-    'l, j F Y - g:ia' => flexible_friendlytime_translate('l, j F Y - g:ia'),
-    'l, Y,  F j - g:ia' => flexible_friendlytime_translate('l, Y,  F j - g:ia'),
-    'l, j. F Y - G:i' => flexible_friendlytime_translate('l, j. F Y - G:i'),
+    2 => elgg_echo('flexible_friendlytime:format:1', array(translate_friendlytime('M j, Y'), translate_friendlytime('g:ia'))),
+    'Y-m-d H:i' => translate_friendlytime('Y-m-d H:i'),
+    'm/d/Y - H:i' => translate_friendlytime('m/d/Y - H:i'),
+    'd/m/Y - H:i' => translate_friendlytime('d/m/Y - H:i'),
+    'Y/m/d - H:i' => translate_friendlytime('Y/m/d - H:i'),
+    'd.m.Y - H:i' => translate_friendlytime('d.m.Y - H:i'),
+    'm/d/Y - g:ia' => translate_friendlytime('m/d/Y - g:ia'),
+    'd/m/Y - g:ia' => translate_friendlytime('d/m/Y - g:ia'),
+    'Y/m/d - g:ia' => translate_friendlytime('Y/m/d - g:ia'),
+    'M j Y - H:i' => translate_friendlytime('M j Y - H:i'),
+    'j M Y - H:i' => translate_friendlytime('j M Y - H:i'),
+    'Y M j - H:i' => translate_friendlytime('Y M j - H:i'),
+    'M j Y - g:ia' => translate_friendlytime('M j Y - g:ia'),
+    'j M Y - g:ia' => translate_friendlytime('j M Y - g:ia'),
+    'Y M j - g:ia' => translate_friendlytime('Y M j - g:ia'),
+    'D, Y-m-d H:i' => translate_friendlytime('D, Y-m-d H:i'),
+    'D, m/d/Y - H:i' => translate_friendlytime('D, m/d/Y - H:i'),
+    'D, d/m/Y - H:i' => translate_friendlytime('D, d/m/Y - H:i'),
+    'D, Y/m/d - H:i' => translate_friendlytime('D, Y/m/d - H:i'),
+    'F j, Y - H:i' => translate_friendlytime('F j, Y - H:i'),
+    'j F, Y - H:i' => translate_friendlytime('j F, Y - H:i'),
+    'Y, F j - H:i' => translate_friendlytime('Y, F j - H:i'),
+    'D, m/d/Y - g:ia' => translate_friendlytime('D, m/d/Y - g:ia'),
+    'D, d/m/Y - g:ia' => translate_friendlytime('D, d/m/Y - g:ia'),
+    'D, Y/m/d - g:ia' => translate_friendlytime('D, Y/m/d - g:ia'),
+    'F j, Y - g:ia' => translate_friendlytime('F j, Y - g:ia'),
+    'j F Y - g:ia' => translate_friendlytime('j F Y - g:ia'),
+    'Y, F j - g:ia' => translate_friendlytime('Y, F j - g:ia'),
+    'j. F Y - G:i' => translate_friendlytime('j. F Y - G:i'),
+    'l, F j, Y - H:i' => translate_friendlytime('l, F j, Y - H:i'),
+    'l, j F, Y - H:i' => translate_friendlytime('l, j F, Y - H:i'),
+    'l, Y,  F j - H:i' => translate_friendlytime('l, Y,  F j - H:i'),
+    'l, F j, Y - g:ia' => translate_friendlytime('l, F j, Y - g:ia'),
+    'l, j F Y - g:ia' => translate_friendlytime('l, j F Y - g:ia'),
+    'l, Y,  F j - g:ia' => translate_friendlytime('l, Y,  F j - g:ia'),
+    'l, j. F Y - G:i' => translate_friendlytime('l, j. F Y - G:i'),
     
   );
 
@@ -82,7 +78,7 @@ foreach($languages as $langpath){
   $field = 'format2' . $language;
   $params['name'] = 'params[format2' . $language . ']';
   $params['value'] = !empty($vars['entity']->$field) ? $vars['entity']->$field : 1;
-  echo elgg_view('input/dropdown', $params);
+  echo elgg_view('input/select', $params);
 
   echo "&nbsp;&nbsp;&nbsp;&nbsp;";
   $field = 'format2override' . $language;
@@ -110,11 +106,10 @@ foreach($languages as $langpath){
     $params['options_values'][$i] = $prefix.$i;
   }
 
-  echo elgg_view('input/dropdown', $params);
+  echo elgg_view('input/select', $params);
 ?>
 
 	</div>
 
 <?php 
 }
-?>
